777c76927f33f656ecd4d3980dfc4ed6027da2ef
Ning Xia <nxiamail@gmail.com>
2015-07-09 17:28:25 -0700

    (fix) correct initial gulpfile errors

---
+++ dev/js/components/app.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <h1>Icarus</h1>
+      </div>
+    );
+  }
+});
+
+module.exports = App;
