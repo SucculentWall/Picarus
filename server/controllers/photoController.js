d57b6c58cd8641157418b7db81a6d42f633b1442
aytang <albertyptang@gmail.com>
2015-07-10 14:39:00 -0700

    (feat) load photos

--- server/controllers/photoController.js
+++ server/controllers/photoController.js
@@ -7,48 +7,57 @@ var fs = require('fs');
 var inspect = require('util').inspect;
 
 module.exports = {
-  
-  addPhoto: function (req, res, next) {
-    
-    var busboy = new Busboy({ headers: req.headers });
 
-    busboy.on('file', function(fieldname, filestream, filename, encoding, mimetype) {
-      console.log('File [' + fieldname + ']: filename: ' + filename);
-      var output = fs.createWriteStream('file.jpg');
+  addPhoto: function (req, res, next) {
+
+    var data = {};
+
+    var busboy = new Busboy({
+      headers: req.headers
+    });
+
+    busboy.on('file', function (fieldname, filestream, filename, encoding, mimetype) {
+      var output = fs.createWriteStream('/photos/testfile' + '.' + data.filetype);
       filestream.pipe(output);
     });
 
-    busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-      console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+    busboy.on('field', function (fieldname, val, fieldnameTruncated, valTruncated) {
+      data[fieldname] = val;
     });
 
-    busboy.on('finish', function() {
-      console.log('Done parsing form!');
-      res.writeHead(303, { Connection: 'close', Location: '/' });
+    busboy.on('finish', function () {
+      console.log('data ', data);
+      new User({
+          username: data.username
+        })
+        .fetch()
+        .then(function (found) {
+          if (!found) {
+            res.send('User not found');
+          } else {
+            new Photo({
+                filename: data.filename,
+                filetype: data.filetype,
+                user_id: found.id
+              })
+              .save();
+          }
+        });
+      res.writeHead(303, {
+        Connection: 'close',
+        Location: '/'
+      });
       res.end();
     });
 
     req.pipe(busboy);
 
-    // new User({username: data.username})
-    //   .fetch()
-    //   .then(function(found){
-    //     if (!found) {
-    //       res.send('User not found');
-    //     } else {
-    //       new Photo({filename: data.filename, filetype: data.filetype, user_id: found.id})
-    //       .save()
-    //       .then(function(created){
-    //         res.send(created);
-    //       });
-    //     }
-    //   });
   },
 
   getAllPhotos: function (req, res, next) {
     Photos.reset()
       .fetch()
-      .then(function(photos){
+      .then(function (photos) {
         console.log(photos);
         res.send(photos.models);
       });
