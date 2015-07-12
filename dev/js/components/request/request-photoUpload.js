513dfa4c17c8609e5e568b0c27b9aa48207bd788
aytang <albertyptang@gmail.com>
2015-07-11 22:38:51 -0700

    (feat) added comments

--- dev/js/components/request/request-photoUpload.js
+++ dev/js/components/request/request-photoUpload.js
@@ -7,23 +7,30 @@ var PhotoUpload = React.createClass({
   },
   _onSubmit: function(e){
     e.preventDefault();
+    // username from ref in form
     var username = this.refs.username.props.value;
+    // request_id from data passed down from app-request
     var request_id = this.props.data.id;
+    // photo from state
     var photo = this.state.photo;
+    // action
     AppActions.addPhoto(photo, username, request_id);
   },
-  handleFile: function(e){
+  _handleFile: function(e){
     // select the event target (the selected image file)
     var file = e.target.files[0];
+    // save to state
     this.setState({
       photo: file
     });
   },
   render: function(){
+    // BOB is the placeholder in the username for now, but any valid username can be submitted
+    // TODO: validation and view refresh
     return (
       <form onSubmit={this._onSubmit} enctype="multipart/form-data">
         <input ref="username" id="req-text" type="text" placeholder="BOB" />
-        <input ref="file" id="req-text" type="file" onChange={this.handleFile} />
+        <input ref="file" id="req-text" type="file" onChange={this._handleFile} />
         <input type="submit" />
       </form>
     );
