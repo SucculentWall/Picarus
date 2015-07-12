603fbf3a31c1d26f29719fe71ac19433b2dbcbec
aytang <albertyptang@gmail.com>
2015-07-11 22:18:34 -0700

    (feat) photo form submission

--- dev/js/components/request/request-photoUpload.js
+++ dev/js/components/request/request-photoUpload.js
@@ -1,13 +1,30 @@
-var React = require("react");
-var AppActions = require("../../actions/app-actions");
+var React = require('react');
+var AppActions = require('../../actions/app-actions');
+var axios = require('axios');
 
 var PhotoUpload = React.createClass({
+  getInitialState: function() {
+    return { photo: null };
+  },
+  _onSubmit: function(e){
+    e.preventDefault();
+    var username = this.refs.username.props.value;
+    var request_id = this.props.data.id;
+    var photo = this.state.photo;
+    AppActions.addPhoto(photo, username, request_id);
+  },
+  handleFile: function(e){
+    // select the event target (the selected image file)
+    var file = e.target.files[0];
+    this.setState({
+      photo: file
+    });
+  },
   render: function(){
     return (
-      <form action="/photos" enctype="multipart/form-data" className="req-form" method="post">
-        <input ref="request_id" id="req-text" type="text" value={this.props.data.id} />
-        <input ref="username" id="req-text" type="text" value="BOB" />
-        <input ref="file" id="req-text" type="file"/>
+      <form onSubmit={this._onSubmit} enctype="multipart/form-data">
+        <input ref="username" id="req-text" type="text" placeholder="BOB" />
+        <input ref="file" id="req-text" type="file" onChange={this.handleFile} />
         <input type="submit" />
       </form>
     );
