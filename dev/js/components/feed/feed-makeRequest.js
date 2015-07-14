dd603c586b6793b3d967f2043030615872ef38be
JP Ji <baka101@gmail.com>
2015-07-14 14:48:44 -0700

    (bug) fix parsing text for hashtag to get tags from requeset text correctly

--- dev/js/components/feed/feed-makeRequest.js
+++ dev/js/components/feed/feed-makeRequest.js
@@ -5,18 +5,17 @@ var MakeRequest = React.createClass({
   _onSubmit: function (e) {
     e.preventDefault();
     var text = React.findDOMNode(this.refs.text).value;
-    // var tags = React.findDOMNode(this.refs.tags).value.split(' ');
 
-
-    // // checks for #thisIsHashtag
+    // checks for #thisIsHashtag
     var tagRegEx = /\S*#(?:\[[^\]]+\]|\S+)/ig;
     var tags = text.match(tagRegEx); // ['#barcelona, #sunset']
-    var refinedTags = tags.map(function(tag){
-      // remove # from tag
-      // should store tags uniformly (eg #dogs and #Dogs should be the same tag)
-      return tag.substr(1).toLowerCase();
-    });
-    // ^--- move into client! 
+    if (tags) {
+      var refinedTags = tags.map(function(tag){
+        // remove # from tag
+        // should store tags uniformly (eg #dogs and #Dogs should be the same tag)
+        return tag.substr(1).toLowerCase();
+      });
+    }
 
     AppActions.addRequest(text, 'BOB', refinedTags); //Hardcoded 'BOB', change later
     React.findDOMNode(this.refs.text).value = '';
@@ -25,8 +24,7 @@ var MakeRequest = React.createClass({
   render: function(){
     return (
       <form className="req-form" onSubmit={this._onSubmit}>
-        <input ref="text" type="text" placeholder="Make a request" />
-        <input ref="tags" type="text" placeholder="Tags(with spaces between)" />
+        <input className="request-input" ref="text" type="text" placeholder="Make a request" />
         <input type="submit" />
       </form>
     );
