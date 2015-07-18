e1afa5ea617964d3a3bdeaaf12f37aae51dbdfe3
Edwin Lin <edwinlin1987@gmail.com>
2015-07-17 18:13:01 -0700

    (feat) testing

---
+++ test/unit/authstore.js
@@ -0,0 +1,20 @@
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+describe('AuthStore', function() {
+  describe('the Authstore methods', function(){
+
+    it('should be an object', function(){
+      expect(AuthStore).to.be.a('object');
+    });
+
+    it('should have all the necessary methods', function(){
+
+      expect(Authstore).to.have.property();
+      expect(Authstore).to.have.property();
+      expect(Authstore).to.have.property();
+    });
+  });
+});
+
---
+++ test/unit/authstore.spec.js
@@ -0,0 +1,17 @@
+var AuthStore = require('../../dev/js/stores/app-authStore');
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+describe('AuthStore', function() {
+  describe('the Authstore methods', function(){
+
+    it('should be an object', function(){
+      expect(AuthStore).to.be.a('object');
+    });
+
+    it('should have methods', function(){
+    });
+  });
+});
+
