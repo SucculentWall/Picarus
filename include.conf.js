66ddf11763d82233be2f58ccc5e9542a5520d8c7
JP Ji <baka101@gmail.com>
2015-07-20 15:59:22 -0700

    (test) create basic setup for unit testing using karma test runner and karma-browserify

---
+++ include.conf.js
@@ -0,0 +1,4 @@
+module.exports = [
+  'dev/js/utils/database-utils.js',
+  'dev/js/stores/app-authStore.js',
+];
--- test/unit/authstore.spec.js
+++ test/unit/authstore.spec.js
@@ -1,16 +1,18 @@
-var assert = require('chai').assert;
-var should = require('chai').should();
-var expect = require('chai').expect;
-require('../../dev/js/main.js');
+var assert = chai.assert;
+var should = chai.should();
+var expect = chai.expect;
+
+var AuthStore = require('../../dev/js/stores/app-authStore');
 
 describe('AuthStore', function() {
   describe('the Authstore methods', function(){
 
     it('should be an object', function(){
-      expect({}).to.be.an('object');
+      expect(AuthStore).to.be.an('object');
     });
 
-    it('should have methods', function(){
+    it('should have the right methods', function(){
+      expect(AuthStore).to.have.property('getId');
     });
   });
 });
