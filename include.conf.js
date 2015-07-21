3c8e5e22cdb0f5efda9d388d0553dcfc796584d8
aytang <albertyptang@gmail.com>
2015-07-21 08:46:57 -0700

    (test) UserStore testing and fixes

--- include.conf.js
+++ include.conf.js
@@ -1,6 +1,7 @@
 module.exports = [
   'dev/js/utils/database-utils.js',
   'dev/js/stores/app-authStore.js',
-  'dev/js/dispatchers/app-dispatcher.js'
+  'dev/js/stores/app-userStore.js',
+  'dev/js/dispatchers/app-dispatcher.js',
   'dev/js/components/feed/feed-makeRequest.js'
 ];
