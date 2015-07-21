d889d8e41e394c63555c8fb23bb2725d22d61fe0
JP Ji <baka101@gmail.com>
2015-07-20 19:29:36 -0700

    (test) set up framework for testing React components

--- include.conf.js
+++ include.conf.js
@@ -2,4 +2,5 @@ module.exports = [
   'dev/js/utils/database-utils.js',
   'dev/js/stores/app-authStore.js',
   'dev/js/dispatchers/app-dispatcher.js'
+  'dev/js/components/feed/feed-makeRequest.js'
 ];
